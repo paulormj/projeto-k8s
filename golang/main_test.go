cb54a689c4cbcd18922343b6bf1e15245f946cea
Paulo Menezes <paulo_rmj@yahoo.com.br>
2020-04-03 12:39:07 -0300

    teste

--- golang/main_test.go
+++ golang/main_test.go
@@ -4,7 +4,7 @@ import (
 )
 func TestGoRocks(t *testing.T){
 	got := greeting("Code.education Rocks!!!")
-	want:= "xxxs!!!"
+	want:= "Code.education Rocks!!!"
 
 	if got != want{
 		t.Errorf("Retornou a mensagem errada.!!")
